752f0cbefc2afc0725ffd4f34bf33acae7b60fb0
ibynx <36119601+ibynx@users.noreply.github.com>
2020-12-04 10:12:07 -0500

    modify for 4-core cpu

--- lib/Cpu.jsx
+++ lib/Cpu.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.jsx";
 const render = ({ output }) => {
   if (typeof output === "undefined") return null;
   return (
-    <div style={output.loadAverage > 3 ? { color: styles.colors.red } : null}>
+    <div style={output.loadAverage > 4 ? { color: styles.colors.red } : null}>
       <span>􀍽 {output.loadAverage}</span>
     </div>
   );
